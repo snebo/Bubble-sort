325dbaf7ee4e9f5ef1237efe2356f9c73b1435b4
Stefano <snebo54@hotmail.com>
2023-12-22 14:59:51 +0100

    Fixed bubble sort for multiple iterations

--- main.rb
+++ main.rb
@@ -1,15 +1,18 @@
 # bubble sort
 example =[4,3,78,2,0,2]
-
+$sorted = false
 def bubble_sort(arr)
-  #first iteration
-  arr.each_index do |i| 
-    if arr[i+1] 
-      if arr[i] > arr[i+1]
-        first = arr[i+1]
-        second = arr[i]
-        arr[i] = first
-        arr[i+1] = second
+  while (not $sorted) do
+  $sorted = true
+    arr.each_index do |i| 
+      if arr[i+1] #continue till the last value
+        if arr[i] > arr[i+1]
+          $sorted = false
+          first = arr[i+1]
+          second = arr[i]
+          arr[i] = first
+          arr[i+1] = second
+        end
       end
     end
   end
